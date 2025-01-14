c0e6c16ddda3ea548eee90e110379a0b5ef24d54
David Vu <phuongvu1794@gmail.com>
2025-01-14 11:14:43 +0700

    add socketIO for server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: ["http://127.0.0.1:5500"],
+    }
+});
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    console.log('connected')
+})
